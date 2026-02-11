1292a7cab1953133458836a7f8e7c744d8c3f66b
furkatovbahrom-code <furkatovbahrom@gmail.com>
2026-02-11 20:19:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.querySelectorAll(".product").forEach(product => {
 
   const price = parseInt(product.dataset.price);
-  const name = product.dataset.name;
-
   const minus = product.querySelector(".minus");
   const plus = product.querySelector(".plus");
   const countEl = product.querySelector(".count");
   const totalEl = product.querySelector(".total-price");
-  const buyBtn = product.querySelector(".buy");
 
   let count = 1;
 
@@ -23,19 +20,6 @@ document.querySelectorAll(".product").forEach(product => {
       countEl.textContent = count;
       totalEl.textContent = count * price;
     }
-  });
-
-  buyBtn.addEventListener("click", () => {
-
-    const total = count * price;
-
-    const message = Сәлеметсіз бе! Мен ${name} өнімінен ${count} дана алғым келеді. Жалпы баға: ${total} ₸;
-
-    const phoneNumber = "77000000000"; // O'ZINGNI NOMERINGNI YOZ
-
-    const url = https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)};
-
-    window.open(url, "_blank");
   });
 
 });
